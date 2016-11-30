f0a08674efde2b1dd3bb7874c3b74c73532d87da
Timothy Redaelli <T.Redaelli@adbglobal.com>
2016-11-30 18:41:56 +0100

    Add support for JavaScript, .NET, REST API, Webhook, File keys

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed,
   clientKey: process.env.CLIENT_KEY || '',
+  javascriptKey: process.env.JAVASCRIPT_KEY || '',
+  dotNetKey: process.env.DOTNET_KEY || '',
+  restAPIKey: process.env.REST_API_KEY || '',
+  webhookKey: process.env.WEBHOOK_KEY || '',
+  fileKey: process.env.FILE_KEY || '',
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
